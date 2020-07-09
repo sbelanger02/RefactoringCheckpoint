05d3df6d69d5c0a03995f9364d34975cf8a12753
sbelanger02 <sbelanger02@gmail.com>
2020-07-09 09:20:46 -0700

    updated class and functions

--- GildedRose.cc
+++ GildedRose.cc
@@ -1,9 +1,84 @@
 #include "GildedRose.h"
+#include <map>
+#include <string.h>
+
+using namespace std;
 
 GildedRose::GildedRose(vector<Item> & items) : items(items)
 {}
-    
-void GildedRose::updateQuality() 
+
+const string GildedRose::_Brie("Aged Brie");
+const string GildedRose::_Backstage("Backstage passes to a TAFKAL80ETC concert");
+const string GildedRose::_Sulfuras("Sulfuras, Hand of Ragnaros");
+
+void GildedRose::updateProperties()
+{
+    for (int i = 0; i < items.size(); i++)
+    {
+        decreaseSellIn(items[i]);
+        updateQualityOfItem(items[i]);
+    }
+}
+
+void GildedRose::updateQualityOfItem(Item& item)
+{
+    typedef void (GildedRose::*FuncPtr)(Item&);
+    map<string, FuncPtr> itemTypeMapper;
+    itemTypeMapper.insert(make_pair(_Brie, &GildedRose::updateBrie));
+    itemTypeMapper.insert(make_pair(_Backstage, &GildedRose::updateBackstage));
+    itemTypeMapper.insert(make_pair(_Sulfuras, &GildedRose::updateSulfuras));
+    map<string, FuncPtr>::const_iterator typeFinder = itemTypeMapper.find(item.name);
+
+    if (typeFinder != itemTypeMapper.end())
+    {
+        FuncPtr funcPtr = typeFinder->second;
+        (this->*funcPtr)(item);
+    }
+    else
+    {
+        defaultUpdate(item);
+    }
+}
+
+void GildedRose::defaultUpdate(Item& item)
+{
+    decreaseQuality(item);
+    if (item.sellIn < 0)
+    {
+        increaseQuality(item);
+    }
+}
+
+void GildedRose::updateSulfuras(Item& item)
+{
+    (void)0;
+}
+
+void GildedRose::increaseQuality(Item& item)
+{
+    if (item.quality < 50)
+    {
+        item.quality = item.quality + 1;
+    }
+}
+
+void GildedRose::decreaseQuality(Item& item)
+{
+    if (item.quality < 0)
+    {
+        item.quality = item.quality - 1;
+    }
+}
+
+void decreaseSellIn(Item& item)
+{
+    if (item.name != _Sulfuras)
+    {
+        item.sellIn = item.sellIn - 1;
+    }
+}
+
+/*void GildedRose::updateQuality() 
 {
     for (int i = 0; i < items.size(); i++)
     {
@@ -77,4 +152,4 @@ void GildedRose::updateQuality()
             }
         }
     }
-}
+} */
--- GildedRose.h
+++ GildedRose.h
@@ -15,10 +15,24 @@ public:
 
 class GildedRose
 {
+    static const string _Brie;
+    static const string _Backstage;
+    static const string _Sulfuras;
+
 public:
     vector<Item> & items;
     GildedRose(vector<Item> & items);
+    void updateProperties();
+private:
+    void decreaseQuality(Item& item);
+    void decreaseSellIn(Item& item);
+    void increaseQuality(Item& item);
+    void updateQualityOfItem(Item& item);
+    void updateBrie(Item& item);
+    void updateBackstage(Item& item);
+    void updateSulfuras(Item& item);
+    void defaultUpdate(Item& item);
     
-    void updateQuality();
 };
+
 
